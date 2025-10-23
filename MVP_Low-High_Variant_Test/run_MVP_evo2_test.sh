8a3e1ce30eb1f23ca8144b8fec92922fc9810c4b
mconery <mitch@Alienware.localdomain>
2025-10-23 14:48:27 -0500

    Changed to 7b 16284-bp context with no parallelization

--- MVP_Low-High_Variant_Test/run_MVP_evo2_test.sh
+++ MVP_Low-High_Variant_Test/run_MVP_evo2_test.sh
@@ -5,19 +5,19 @@
 #SBATCH -t 6:00:00
 
 ################################################################################################################
-########################## Define directories and other key file/script locations ##############################
-################################################################################################################
-out_file="/vast/projects/anuragv/cohort/mconery/mvp_variant_test/MVP_variant_scores.1b_model.16284bp_context.csv"
-evotwo_script="/vast/home/m/mconery/Evo2-TopMed-Variant-Scoring/MVP_Low-High_Variant_Test/mvp_variants_test.py"
-
-################################################################################################################
 ########################################## Define key model parameters #########################################
 ################################################################################################################
 chunk_size=100
-window_size=8192
+window_size=16284
 MODEL_SIZE="7b"
 tp_size=1
 cp_size=1
+
+################################################################################################################
+########################## Define directories and other key file/script locations ##############################
+################################################################################################################
+out_file=/vast/projects/anuragv/cohort/mconery/mvp_variant_test/MVP_variant_scores."$MODEL_SIZE"_model."$window_size"bp_context.csv
+evotwo_script="/vast/home/m/mconery/Evo2-TopMed-Variant-Scoring/MVP_Low-High_Variant_Test/mvp_variants_test.py"
 
 ################################################################################################################
 ############################################# Call Evo2 Scoring Script #########################################
