2523970eae7d1f00d23146dcc71f50337b65b7dc
Mitch Conery <mconery@login02.cm.cluster>
2026-03-13 17:55:47 -0400

    Deleted junk files

--- testing/test_fasta_generator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-length = 100000  # 100 kb
-seq = ''.join(random.choices('ATCG', k=length))
-with open('sequence_1kb.fasta', 'w') as f:
-    f.write('>synthetic_100kb_sequence\n')
-    for i in range(0, len(seq), 70):
-        f.write(seq[i:i+70] + '\n')
